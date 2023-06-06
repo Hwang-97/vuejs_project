e6a1bee4b3b21fc96c4258eb1534a039e8c8e7e1
hhw <hhw@hhwui-MacBookPro.local>
2023-06-06 22:16:20 +0900

    delete gradle project

--- back/src/main/java/com/hhw/api/test2/repository/BoardDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hhw.api.test2.repository;
-
-import com.hhw.api.test2.entity.Board;
-import com.hhw.api.test2.entity.BoardDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardDetailRepository extends JpaRepository<BoardDetail, Integer> {
-}
