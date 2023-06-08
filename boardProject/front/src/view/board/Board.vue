98e1eae9e2691b6a7b95a58f480eaae1b7574f5c
Hwang-97 <qqq4472@gmail.com>
2023-06-09 02:46:44 +0900

    테스트용 코드 제거

--- boardProject/front/src/view/board/Board.vue
+++ boardProject/front/src/view/board/Board.vue
@@ -121,7 +121,6 @@ export default {
           let tmpSize = Number(res.data[0]);
           tmpSize = Math.ceil(tmpSize/10);
           this.pageData.totPage = tmpSize;
-          this.pageData.page = 1;
           this.list = res.data.slice(1);
         } else {
           alert("데이터가 존재하지 않습니다.");
